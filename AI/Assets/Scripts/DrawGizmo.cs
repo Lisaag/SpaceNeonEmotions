7480cd75dab492fd52bb002b5441ce5438cbcd62
Dennis Boon <dennis.boon96@gmail.com>
2019-10-30 10:46:10 +0100

    Added AI project

---
+++ AI/Assets/Scripts/DrawGizmo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawGizmo : MonoBehaviour
+{
+    public bool isButtonPosition;
+    public GameObject attachedObject;
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(this.transform.position, 0.1f);
+    }
+}
