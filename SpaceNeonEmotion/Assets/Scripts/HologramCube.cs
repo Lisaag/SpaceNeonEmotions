f9a8bcd955a403851ecb3c7b18aae8a9b4f03a67
EnergySlam <anijsbroodje@gmail.com>
2019-10-31 14:53:54 +0100

    Added hologram shader and box with some functionality

---
+++ SpaceNeonEmotion/Assets/Scripts/HologramCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramCube : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("CubeLocation"))
+        {
+            Destroy(collision.gameObject);
+            this.transform.rotation = collision.gameObject.transform.rotation;
+            this.GetComponent<Rigidbody>().isKinematic = true;
+            this.transform.position = collision.transform.position;
+            gameObject.AddComponent<CubeRotator>();
+        }
+    }
+}
