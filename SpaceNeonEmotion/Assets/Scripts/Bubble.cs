758bd1bad0bf40c5312e66caf69b2910f4447e39
TychoLaptop\iMrBa_000 <TychoZaal@gmail.com>
2019-10-31 15:53:48 +0100

    Bubbles + model

---
+++ SpaceNeonEmotion/Assets/Scripts/Bubble.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    public float speedY, strength, speed;
+
+    private void Start()
+    {
+        speedY = Random.Range(0.01f, 0.03f);
+        var scale = Random.Range(0.05f, 0.17f);
+        transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        strength = Random.Range(0.01f, 0.03f);
+        transform.position += (Mathf.Sin(2 * Mathf.PI * speed * Time.time) - Mathf.Sin(2 * Mathf.PI * speed * (Time.time - Time.deltaTime))) * transform.forward * strength;
+        strength = Random.Range(0.01f, 0.03f);
+        transform.position += (Mathf.Sin(2 * Mathf.PI * speed * Time.time) - Mathf.Sin(2 * Mathf.PI * speed * (Time.time - Time.deltaTime))) * transform.right * strength;
+        transform.position += new Vector3(0, speedY / 2, 0);
+    }
+}
---
+++ SpaceNeonEmotion/Assets/Scripts/SpawnBubbles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBubbles : MonoBehaviour
+{
+    public int amountToSpawn = 10;
+    public GameObject bubble;
+    public Vector3 offset;
+
+    public float horMin, horMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < amountToSpawn; i++)
+        {
+            offset.x = Random.Range(horMin, horMax);
+            offset.z = Random.Range(horMin, horMax);
+            Instantiate(bubble, transform.position += offset, Quaternion.identity);
+        }
+    }
+}
