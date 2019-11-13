669e6f5160dad2605eb0da483f7d9f028be82e67
TychoLaptop\iMrBa_000 <TychoZaal@gmail.com>
2019-11-13 11:59:36 +0100

    Color lerp & support for never having a tutorial (dev)

--- SpaceNeonEmotion/Assets/TutorialManager.cs
+++ SpaceNeonEmotion/Assets/TutorialManager.cs
@@ -12,9 +12,16 @@ public class TutorialManager : MonoBehaviour
 
     public List<TeleportPoint> teleportPoints;
 
+    public Vector3 tpPointPos1, tpPointPos2, tpPointPos3;
+
     public Material highlighted, regular;
 
+    private float startTime;
+    public float speed = 3;
+
     public GameObject thumbButton, indexButton, controller;
+
+    public Color startColor, endColor;
 
     public Teleport teleport;
 
@@ -27,7 +34,7 @@ public class TutorialManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 1; i < phases.Count; i++) // for each phase
+        for (int i = 0; i < phases.Count; i++) // for each phase
         {
             for (int j = 0; j < phases[i].phaseObjects.Count; j++) // for each object in each phase
             {
@@ -48,6 +55,7 @@ public class TutorialManager : MonoBehaviour
         }
 
         thumbButton.GetComponent<Renderer>().material = highlighted;
+        startTime = Time.time;
     }
 
     // Update is called once per frame
@@ -62,47 +70,59 @@ public class TutorialManager : MonoBehaviour
         {
             NextPhase();
         }
+
+        float time = (Mathf.Sin(Time.time - startTime) * speed);
+        highlighted.color = Color.Lerp(startColor, endColor, time);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            NextPhase();
+        }
     }
 
     public void NextPhase()
     {
-        for (int i = 0; i < phases[currentPhase].phaseObjects.Count; i++)
+        if (currentPhase <= 2)
         {
-            phases[currentPhase].phaseObjects[i].SetActive(false);
-        }
+            for (int i = 0; i < phases[currentPhase].phaseObjects.Count; i++)
+            {
+                phases[currentPhase].phaseObjects[i].SetActive(false);
+            }
 
-        currentPhase++;
+            currentPhase++;
 
-        for (int i = 0; i < phases[currentPhase].phaseObjects.Count; i++)
-        {
-            phases[currentPhase].phaseObjects[i].SetActive(true);
-        }
+            for (int i = 0; i < phases[currentPhase].phaseObjects.Count; i++)
+            {
+                phases[currentPhase].phaseObjects[i].SetActive(true);
+            }
 
-        switch(currentPhase)
-        {
-            case 0:
-                // use start method instead
-                break;
-            case 1:
-                teleportArea.enabled = false;
-                break;
-            case 2:
-                for (int i = 0; i < teleportPoints.Count; i++)
-                {
-                    teleportPoints[i].gameObject.SetActive(false);
-                    teleportPoints[i].enabled = false;
-                }
+            switch (currentPhase)
+            {
+                case 0:
+                    // use start method instead
+                    break;
+                case 1:
+                    teleportArea.enabled = false;
+                    teleportArea.gameObject.SetActive(false);
+                    break;
+                case 2:
+                    for (int i = 0; i < teleportPoints.Count; i++)
+                    {
+                        teleportPoints[i].gameObject.SetActive(false);
+                        teleportPoints[i].enabled = false;
+                    }
 
-                thumbButton.GetComponent<Renderer>().material = regular;
-                indexButton.GetComponent<Renderer>().material = highlighted;
-                break;
-            case 3:
-                teleportArea.gameObject.SetActive(true);
-                teleportArea.enabled = true;
-                controller.SetActive(false);
-                break;
-            case 4:
-                break;
+                    thumbButton.GetComponent<Renderer>().material = regular;
+                    indexButton.GetComponent<Renderer>().material = highlighted;
+                    break;
+                case 3:
+                    teleportArea.gameObject.SetActive(true);
+                    teleportArea.enabled = true;
+                    controller.SetActive(false);
+                    break;
+                case 4:
+                    break;
+            }
         }
     }
 }
