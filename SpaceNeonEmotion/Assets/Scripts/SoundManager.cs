3bc44681a0f6e248a23bd514ea7b0cbc96712ab1
TychoDesktop <TychoZaal@gmail.com>
2019-11-21 18:34:41 +0100

    excuse me, but what the frick is a delegate.

---
+++ SpaceNeonEmotion/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviourExtensionCoroutines
+{
+    public static SoundManager instance;
+    public Action<AudioSource, Transform, bool> ikhebtitled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void PlaySound(AudioSource clip, Transform position, bool loop, int delayInSeconds)
+    {
+        this.StartCoroutine(() =>
+        {
+           gameObject.SetActive(false);
+        }, delayInSeconds);
+    }
+}
