8728c2780dbe83f354076fb8d61daae612d0de17
Dennis Boon <dennis.boon96@gmail.com>
2019-12-09 18:42:40 +0100

    Made first version of testing scense: - blue and red scenes -Survey buttons, checks if user is touching button and if vive controller button is pressed -Scriptable object keeps track of survey results of both rooms seperately

---
+++ SpaceNeonEmotion/Assets/Scripts/TestScene/SurveyManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SurveyManager : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> disableObjects = new List<GameObject>();
+
+    [SerializeField]
+    GameObject buttons;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            foreach(GameObject g in disableObjects)
+            {
+                g.SetActive(false);
+            }
+            buttons.SetActive(true);
+        }
+    }
+}
