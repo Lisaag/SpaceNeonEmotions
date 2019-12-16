22b57a164e31ac425a9ca95b890e9a044b6da6d5
Lisa <lisatgroen@hotmail.com>
2019-12-16 14:46:06 +0100

    Added graph to project

---
+++ SpaceNeonEmotion/Assets/Scripts/Graph/BaseLine.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseLine : MonoBehaviour
+{
+    [SerializeField]
+    MeshFilter mf;
+
+    private const int lineDetail = 30;
+
+    [SerializeField]
+    private float lineHeight;
+
+    [SerializeField]
+    private float lineThickness;
+
+    List<Vector3> linePoints = new List<Vector3>();
+    Vector3[] vertices = new Vector3[lineDetail * 2];
+    private Mesh mesh;
+
+    private void Start()
+    {
+        Debug.Log("vertices size: " + vertices.Length);
+        mesh = GetComponent<MeshFilter>().mesh;
+        CalculateLinePoints();
+        DrawTriangles();
+    }
+
+    void CalculateLinePoints()
+    {
+        for (int i = 0; i < lineDetail; i++)
+        {
+            Debug.Log(i + lineDetail);
+            float radius = 5.0f;
+            float xPos = this.transform.position.x - radius * Mathf.Sin((Mathf.PI / lineDetail) * i);
+            float zPos = this.transform.position.z - radius * Mathf.Cos((Mathf.PI / lineDetail) * i);
+            linePoints.Add(new Vector3(xPos, lineHeight, zPos));
+            vertices[i] = new Vector3(xPos, lineHeight, zPos);
+            vertices[i + lineDetail] = new Vector3(xPos, lineHeight - lineThickness, zPos);
+        }
+
+        mesh.vertices = vertices;
+    }
+
+    void DrawTriangles()
+    {
+        List<int> triangles = new List<int>();
+
+        for (int i = 0; i < lineDetail - 1; i++)
+        {
+            triangles.Add(i);
+            triangles.Add(lineDetail + i + 1);
+            triangles.Add(lineDetail + i);
+
+            triangles.Add(i);
+            triangles.Add(i + 1);
+            triangles.Add(lineDetail + i + 1);
+        }
+
+        for (int i = 0; i < lineDetail - 1; i++)
+        {
+            triangles.Add(i);
+            triangles.Add(lineDetail + i);
+            triangles.Add(lineDetail + i + 1);
+
+            triangles.Add(i);
+            triangles.Add(lineDetail + i + 1);
+            triangles.Add(i + 1);
+        }
+
+        mesh.triangles = triangles.ToArray();
+    }
+}
---
+++ SpaceNeonEmotion/Assets/Scripts/Graph/LerpColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpColor : MonoBehaviour
+{
+    [SerializeField]
+    MeshRenderer meshRenderer;
+
+    [SerializeField]
+    Gradient gradient;
+
+    [SerializeField]
+    float alphaValue;
+
+    private void Start()
+    {
+        Color color = gradient.Evaluate(transform.localPosition.y / 6.0f);
+        gameObject.GetComponent<MeshRenderer>().sharedMaterial.SetColor("_BaseColor", new Color(color.r, color.g, color.b, alphaValue));
+        meshRenderer.material.color = new Color(color.r, color.g, color.b, alphaValue);
+
+        Debug.Log("changing graph point color");
+    }
+}
