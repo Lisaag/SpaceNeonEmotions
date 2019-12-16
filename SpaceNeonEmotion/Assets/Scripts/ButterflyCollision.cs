5afecccc7a117ed3b5ab86a7e16155b3265cf8ca
Dennis Boon <dennis.boon96@gmail.com>
2019-12-16 16:56:11 +0100

    butterfly dingetjes

---
+++ SpaceNeonEmotion/Assets/Scripts/ButterflyCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyCollision : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Butterfly"))
+        {
+            if (!audioSource.isPlaying)
+                SoundManager.instance.PlaySound(audioSource, other.gameObject, false, 0);
+        }
+    }
+}
