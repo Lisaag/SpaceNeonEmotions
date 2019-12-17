4caba63e098915bde5d65e20f03ad81425f0f8d1
Lisa <lisatgroen@hotmail.com>
2019-12-17 12:13:12 +0100

    added survey scripts to project

---
+++ SpaceNeonEmotion/Assets/Scripts/Graph/ButtonPresses.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Button Presses", menuName = "buttn Presses")]
+public class ButtonPresses : ScriptableObject
+{
+    public List<int> blueRoom = new List<int>();
+    public List<int> redRoom = new List<int>();
+}
---
+++ SpaceNeonEmotion/Assets/Scripts/Graph/Survey.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using Valve.VR;
+using System;
+using UnityEngine.SceneManagement;
+
+public class Survey : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource audioSource;
+
+    [SerializeField]
+    Interactable interactable;
+
+    [SerializeField]
+    int buttonIndex;
+
+    [SerializeField]
+    ButtonPresses buttonPresses;
+
+    [SerializeField]
+    SteamVR_Action_Boolean grabPinch;
+
+    SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;
+
+
+
+    private bool isPressed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (grabPinch != null)
+        {
+            grabPinch.AddOnChangeListener(OnTriggerPressedOrReleased, inputSource);
+        }
+    }
+
+    private void OnTriggerPressedOrReleased(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool newState)
+    {
+        if (interactable.isHovering && !isPressed)
+        {
+            isPressed = true;
+            audioSource.Play();
+            StartCoroutine(WaitAndDisable());
+            if (SceneManager.GetActiveScene().name == "TestRed")
+            {
+                buttonPresses.redRoom.Add(buttonIndex);
+            }
+            else
+            {
+                buttonPresses.blueRoom.Add(buttonIndex);
+            }
+        }
+    }
+
+    private IEnumerator WaitAndDisable()
+    {
+        yield return new WaitForSeconds(0.5f);
+        transform.parent.gameObject.SetActive(false);
+    }
+}
