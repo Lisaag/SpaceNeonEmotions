ab96dd2d3f2283cc720c4b14aaf6a7421420b115
Dennis Boon <dennis.boon96@gmail.com>
2020-01-16 19:20:01 +0100

    Added new script for planets, removed interactables, re-added collision

--- SpaceNeonEmotion/Assets/SteamVR/InteractionSystem/Teleport/Scripts/SoundManager.cs
+++ SpaceNeonEmotion/Assets/SteamVR/InteractionSystem/Teleport/Scripts/SoundManager.cs
@@ -27,7 +27,6 @@ public class SoundManager : MonoBehaviourExtensionCoroutines
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
         if (SoundManager.instance == null)
             instance = this;
         else
