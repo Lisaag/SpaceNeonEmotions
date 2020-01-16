d13219ac0a2bc25f60df4e7feef6e64651738f6e
Dennis Boon <dennis.boon96@gmail.com>
2020-01-16 15:03:36 +0100

    DontDestroy gekut

---
+++ SpaceNeonEmotion/Assets/SteamVR/InteractionSystem/Teleport/Scripts/ChangeScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public string newScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {;
+            if (SceneManager.GetActiveScene().name == "FinalMenu")
+            {
+                Destroy(GameObject.Find("FirstRoom"));
+                Destroy(GameObject.Find("SoundManager"));
+            }
+            SceneManager.LoadScene(newScene);
+        }
+    }
+}
